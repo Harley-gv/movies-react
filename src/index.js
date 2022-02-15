dd84d26ea70b3cd52c33055b9c7779f3479562c3
Harley-gv <hegv1995@gmail.com>
2022-02-14 22:16:12 -0500

    react app de peliculas version 1.0

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Home } from "./home";
 
-
+// renderizamos vistas del clienteS
 ReactDOM.render(
   <Home />,
   document.getElementById('root')
 );
-//
